db58efdd91d6963ccbc5d23ba4820fef03a44a4b
Peter Fonseca <peter.nfonseca@gmail.com>
2018-02-04 08:26:49 -0800

    TypeScriptify

---
+++ index.ts
@@ -0,0 +1,9 @@
+import './styles.scss'
+
+function logWithEmoji(message: string) {
+  const emojis = ['🎉', '📦', '🎊']
+  const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+  console.log(`${message} ${randomEmoji}`);
+}
+
+logWithEmoji('Hello from the typescripts!');
